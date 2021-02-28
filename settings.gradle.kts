2479cb4f7f07b4ef2b8d288852aa51897525fb7d
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-02-28 12:14:50 +0100

    finish - kotlin dsl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,13 +12,51 @@ dependencyResolutionManagement {
     }
 
     versionCatalogs {
+        val activity = "1.2.0"
+        val appcompat = "1.2.0"
+        val coroutines = "1.4.2"
+        val fragment = "1.3.0"
+        val material = "1.3.0"
+        val recyclerView = "1.1.0"
+        val retrofit = "2.9.0"
+
         create("androidLibs") {
-            from(files("gradle/shared.versions.toml"))
-            from(files("gradle/android.libs.versions.toml"))
+            alias("activity").to("androidx.activity:activity:$activity")
+            alias("appcompat").to("androidx.appcompat", "appcompat").version(appcompat)
+            alias("coroutines")
+                .to("org.jetbrains.kotlinx", "kotlinx-coroutines-android")
+                .version(coroutines)
+            alias("fragment").to("androidx.fragment", "fragment-ktx").version(fragment)
+            alias("material").to("com.google.android.material", "material").version(material)
+            alias("recyclerView").to("androidx.recyclerview", "recyclerview").version(recyclerView)
+
+            bundle(
+                "feature",
+                listOf(
+                    "appcompat",
+                    "coroutines",
+                    "fragment",
+                    "material",
+                ),
+            )
         }
         create("jvmLibs") {
-            from(files("gradle/shared.versions.toml"))
-            from(files("gradle/jvm.libs.versions.toml"))
+            alias("coroutines")
+                .to("org.jetbrains.kotlinx", "kotlinx-coroutines-core")
+                .version(coroutines)
+            alias("retrofit-retrofit").to("com.squareup.retrofit2", "retrofit").version(retrofit)
+            alias("retrofit-moshi")
+                .to("com.squareup.retrofit2", "converter-moshi")
+                .version(retrofit)
+
+            bundle(
+                "network",
+                listOf(
+                    "coroutines",
+                    "retrofit-retrofit",
+                    "retrofit-moshi",
+                ),
+            )
         }
     }
 }
